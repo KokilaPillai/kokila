39128bda302ebdab042596006e1b315634ebf4cf
KokilaPillai <kokipillai9@gmail.com>
2021-06-27 22:48:17 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+package isa2jenkins;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter two numbers");
+	    int first = 59;
+	    int second = 1;
+	    
+	    System.out.println(first + " " + second);
+
+	    // Add two numbers
+	    int sum = first + second;
+	    System.out.println("After adding The sum is: " + sum);
+	    
+	    // Difference of two numbers
+	    int difference = first - second;
+	    System.out.println("After  Subtraction the answer is: " + difference);
+	    
+	    // Multiplication of two numbers
+	    int multiplication = first - second;
+	    System.out.println("After Multiplication answer is: " + multiplication);
+	    
+
+	}
+
+}
